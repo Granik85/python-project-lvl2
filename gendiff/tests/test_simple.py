20235ce7934fd1f6b40d0a5dca6ba21bb4a7a70a
Artem Ivanov <wertart@gmail.com>
2021-06-16 18:26:57 +0500

    add test

--- gendiff/tests/test_simple.py
+++ gendiff/tests/test_simple.py
@@ -2,5 +2,6 @@ from gendiff.gendiff import generate_diff
 
 
 def test_simple1():
-    diff = generate_diff('gendiff/tests/test_file1.json', 'gendiff/tests/test_file2.json')
+    diff = generate_diff('gendiff/tests/test_file1.json',
+                         'gendiff/tests/test_file2.json')
     assert diff == '{\n    host: "hexlet.io"\n}'
