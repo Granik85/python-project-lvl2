870bb183822685c378ff7a56bdafabc6060286f5
Artem Ivanov <wertart@gmail.com>
2021-06-16 14:52:29 +0500

    added module

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 
 def main():
@@ -8,3 +9,28 @@ def main():
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     print(args)
+    generate_diff(args.first_file, args.second_file)
+
+
+def generate_diff(file_path1, file_path2):
+    str_result = '{\n'
+    json_minus = json.load(open(file_path1))
+    json_plus = json.load(open(file_path2))
+    print(type(json_minus))
+    keys = set(json_plus)
+    keys.update(set(json_minus))
+    for item in sorted(keys):
+        if json_minus.get(item) is None:
+            str_result += '  + ' + item + ': ' + json.dumps(json_plus.get(item)) + '\n'
+        elif json_plus.get(item) is None:
+            str_result += '  - ' + item + ': ' + json.dumps(json_minus.get(item)) + '\n'
+        elif json_minus.get(item) != json_minus.get(item):
+            str_result += '  - ' + item + ': ' + json.dumps(json_minus.get(item)) + '\n'
+            str_result += '  + ' + item + ': ' + json.dumps(json_plus.get(item)) + '\n'
+        else:
+            str_result += '    ' + item + ': ' + json.dumps(json_plus.get(item)) + '\n'
+    str_result += '}'
+    print(str_result)
+
+if __name__ == '__main__':
+    main()
