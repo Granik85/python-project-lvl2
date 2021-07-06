d77a5de9f5560854176481c60085b00c1918251d
Artem Ivanov <wertart@gmail.com>
2021-07-07 00:28:56 +0500

    add parsing module

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
-import json
+
+from .parsing import generate_diff
 
 
 def main():
@@ -14,30 +15,7 @@ def main():
     print(diff)
 
 
-def generate_diff(file_path1, file_path2):
-    str_result = '{\n'
-    json_minus = json.load(open(file_path1))
-    json_plus = json.load(open(file_path2))
-    print(type(json_minus))
-    keys = set(json_plus)
-    keys.update(set(json_minus))
-    for item in sorted(keys):
-        if json_minus.get(item) is None:
-            str_result += '  + ' + item + \
-                          ': ' + json.dumps(json_plus.get(item)) + '\n'
-        elif json_plus.get(item) is None:
-            str_result += '  - ' + item + \
-                          ': ' + json.dumps(json_minus.get(item)) + '\n'
-        elif json_minus.get(item) != json_plus.get(item):
-            str_result += '  - ' + item + \
-                          ': ' + json.dumps(json_minus.get(item)) + '\n'
-            str_result += '  + ' + item + \
-                          ': ' + json.dumps(json_plus.get(item)) + '\n'
-        else:
-            str_result += '    ' + item + \
-                          ': ' + json.dumps(json_plus.get(item)) + '\n'
-    str_result += '}'
-    return str_result
+
 
 
 if __name__ == '__main__':
---
+++ gendiff/parsing.py
@@ -0,0 +1,42 @@
+import json
+
+
+def generate_diff(file_path1, file_path2):
+    type_comparison = check_file_type(file_path1)
+    if type_comparison == 'json':
+        str_result = '{\n'
+        json_minus = json.load(open(file_path1))
+        json_plus = json.load(open(file_path2))
+        print(type(json_minus))
+        keys = set(json_plus)
+        keys.update(set(json_minus))
+        for item in sorted(keys):
+            if json_minus.get(item) is None:
+                str_result += '  + ' + item + \
+                              ': ' + json.dumps(json_plus.get(item)) + '\n'
+            elif json_plus.get(item) is None:
+                str_result += '  - ' + item + \
+                              ': ' + json.dumps(json_minus.get(item)) + '\n'
+            elif json_minus.get(item) != json_plus.get(item):
+                str_result += '  - ' + item + \
+                              ': ' + json.dumps(json_minus.get(item)) + '\n'
+                str_result += '  + ' + item + \
+                              ': ' + json.dumps(json_plus.get(item)) + '\n'
+            else:
+                str_result += '    ' + item + \
+                              ': ' + json.dumps(json_plus.get(item)) + '\n'
+        str_result += '}'
+        return str_result
+    elif type_comparison == 'yaml':
+        pass #yaml loader and handler will be here
+
+
+
+
+
+
+def check_file_type(file_path:str):
+    if file_path.endswith('json'):
+        return 'json'
+    if file_path.endswith('yaml') or file_path.endswith('yml'):
+        return 'yaml'
