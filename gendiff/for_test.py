87f200edbcef94bdb94cf9a47c7df2d2381c34c8
Artem Ivanov <wertart@gmail.com>
2021-10-11 17:33:34 +0500

    step 6

--- gendiff/for_test.py
+++ gendiff/for_test.py
@@ -4,5 +4,7 @@ from stylish import stylish
 t = generate_diff2('./tests/fixtures/step6_1.json',
               './tests/fixtures/step6_2.json')
 #print(t)
-print(stylish(generate_diff('./tests/fixtures/step6_1.json',
-              './tests/fixtures/step6_2.json')))
+#print(stylish(generate_diff('./tests/fixtures/step6_1.json',
+             # './tests/fixtures/step6_2.json')))
+print(stylish(generate_diff('./tests/fixtures/step6_base1.yaml',
+              './tests/fixtures/step6_base2.yaml')))
