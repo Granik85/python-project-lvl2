07d08f1f9a8c1c805bcf6ad0bd52b3a5d39dc188
Artem Ivanov <wertart@gmail.com>
2021-10-11 18:10:06 +0500

    step 6

--- gendiff/formatters/format_diff.py
+++ gendiff/formatters/format_diff.py
@@ -1,6 +1,6 @@
 from gendiff.formatters.stylish import stylish
 from gendiff.formatters.plain import plain
-# from gendiff.formatters.json import format_json
+from gendiff.formatters.json import format_json
 
 
 def format_diff(list_diff, format_name):
@@ -9,6 +9,6 @@ def format_diff(list_diff, format_name):
         return stylish(list_diff)
     elif format_name == 'plain':
         return plain(list_diff)
-#    elif format_name == 'json':
-#        return format_json(list_diff)
+    elif format_name == 'json':
+        return format_json(list_diff)
     raise 'Format not found!'
